cd60189f343734b67fc5e88b223fb8a57c0e1338
vladiuse <vladiuse@gmail.com>
2023-11-06 14:54:11 +0300

    add main parse file

---
+++ adslib_parser.py
@@ -0,0 +1,72 @@
+import argparse
+from configparser import ConfigParser
+from parser.loger import TxtLogger
+from run_parse import test_driver, run_adslib_parser
+
+config_file_path = './conf.ini'
+config = ConfigParser()
+config.read(config_file_path)
+log_file_path = config.get('Loger' ,'log_file_path')
+
+
+
+parser = argparse.ArgumentParser(description='xxxx')
+parser.add_argument('command', type=str, help='Запуск команды')
+parser.add_argument('-proxy', type=str, default=None, help='Proxy id')
+parser.add_argument('-country', type=str, default=None, help='Countryd')
+parser.add_argument('-language', type=str, default=None, help='Language of keywords')
+
+
+def run_parser(*args,**kwargs):
+    print('RUN PARSER')
+    print(args)
+    print(kwargs)
+
+
+def run_logee(*args,**kwargs):
+    print('RUN loger')
+    print(args)
+    print(kwargs)
+
+
+available_commands = {
+    'parse': run_parser,
+    'parse_stat': run_logee,
+}
+args = parser.parse_args()
+
+txt_loger = TxtLogger(log_file_path)
+if args.command == 'parse':
+    if not args.country:
+        country = config.get('KeyWord', 'country')
+    else:
+        country = args.country
+    if not args.language:
+        language = config.get('KeyWord', 'language')
+    else:
+        language = args.language
+    start_key = config.get('KeyWord', 'start_key')
+    end_key = config.get('KeyWord', 'end_key')
+    if args.proxy:
+        try:
+            proxy = config['Proxy'][args.proxy]
+        except KeyError:
+            print('Incorrect proxy id')
+            exit()
+    else:
+        proxy = None
+    # test_driver(txt_loger,country=country, language=language, proxy=proxy,
+    #            keys_range=(start_key,end_key),
+    #            )
+    run_adslib_parser(txt_loger, country=country, language=language, proxy=proxy,
+                keys_range=(start_key, end_key),
+                )
+elif args.command == 'parse_stat':
+    txt_loger.log_file_stat()
+else:
+    print('Incorrect command')
+    print('Available_commands:', list(available_commands.keys()))
+
+
+
+
