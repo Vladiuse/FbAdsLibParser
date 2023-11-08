2af11accc1a8da22d901e6840f28405a054bc48e
vladiuse <vladiuse@gmail.com>
2023-11-08 19:19:08 +0300

    add pinger

---
+++ parser/pinger/main.py
@@ -0,0 +1,53 @@
+import requests as req
+from config import config
+from requests.exceptions import RequestException
+from print_color import print as cprint
+
+DOMAIN =  config.get('Pinger', 'domain')
+URL = config.get('Pinger', 'ping_url')
+
+class PingeReqError(Exception):
+
+    TEXT_COLOR = 'red'
+
+    PLAY_SOUND = config.get('AdsLibParser', 'play_sound')
+    SHOW_COLORS = config.get('AdsLibParser', 'show_colors')
+
+    def __init__(self, error_text):
+        self.error_text = error_text
+
+    def __call__(self):
+        if PingeReqError.SHOW_COLORS == 'true':
+            cprint(f'[Pinger] \n{self.error_text}', color=self.TEXT_COLOR)
+        else:
+            print(f'[Pinger] {self.error_text}')
+
+
+
+class Pinger:
+    PC_NAME = config.get('Pc', 'name')
+    PING_URL = f'http://{DOMAIN}/{URL}/{PC_NAME}/'
+    PING_TRY = 3
+
+
+    def __call__(self):
+        for _ in range(Pinger.PING_TRY):
+            try:
+                self.ping()
+            except PingeReqError as error:
+                error()
+            else:
+                break
+
+    def ping(self):
+        try:
+            res = req.get(Pinger.PING_URL)
+            if res.status_code == 200:
+                print('[Pinger] success')
+            else:
+                raise PingeReqError('status code not 200')
+        except RequestException as error:
+            error_text = str(error)
+            raise PingeReqError(error_text)
+
+
