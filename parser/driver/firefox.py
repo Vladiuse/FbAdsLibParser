4395d85cb20b45529f18bc4590b9055c822773e1
vladiuse <vladiuse@gmail.com>
2023-11-15 23:14:13 +0300

    ADD firefox get_driver global NOT WORK!!!

--- parser/driver/firefox.py
+++ parser/driver/firefox.py
@@ -1,34 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.options import Options
 from seleniumwire import webdriver as wire_webdriver
-#from config import config
 
-def firefox_driver(proxy=None):
-    seleniumwire_options = {
-    'proxy': {
-        'http': proxy,
-        'https': proxy,
-        }
-    }
-
-    firefox_profile = FirefoxProfile()
-    #firefox_profile.set_preference("javascript.enabled", False)
+def get_driver(proxy=None):
     service = webdriver.FirefoxService(
-        service_args=['--profile-root', './firefox_profile'],
+        service_args=['--profile-root', '.'],
     )
-    options = webdriver.FirefoxOptions()
-    #options.binary_location = '/usr/local/bin/geckodriver'
-    options.profile = firefox_profile
+    options = Options()
     # options.add_argument('--headless')
-    DRIVER = wire_webdriver.Firefox(
-        service=service,
-        seleniumwire_options=seleniumwire_options,
-    )
+    if proxy:
+        seleniumwire_options = {
+        'proxy': {
+            'http': proxy,
+            'https': proxy,
+            }
+        }
+
+        DRIVER = wire_webdriver.Firefox(
+            service=service,
+            seleniumwire_options=seleniumwire_options,
+            options=options,
+        )
+    else:
+        DRIVER = webdriver.Firefox(
+            service=service,
+            options=options,
+        )
+
     return DRIVER
 
+if __name__ == '__main__':
+    driver = get_driver(
+        #proxy='http://kEJANU:AN6rAD6ur3Ca@lu.mobileproxy.space:1249'
+    )
+    driver.get('https://api.ipify.org?format=html')
+    print(driver.page_source)
+    input()
 # 119.0.1
 
-if __name__ == '__main__':
-    driver = firefox_driver(proxy='http://kEJANU:AN6rAD6ur3Ca@lu.mobileproxy.space:1249')
-    driver.get('https:/google.com/')
-    input()
